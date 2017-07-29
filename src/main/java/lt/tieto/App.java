87fda83f4b0f8f99e6f6a849fbaaac8150760d62
Justas Ragauskas <justas.ragauskas@gmail.com>
2017-07-30 01:03:02 +0300

    Initial commit

---
+++ src/main/java/lt/tieto/App.java
@@ -0,0 +1,15 @@
+package lt.tieto;
+
+import static spark.Spark.get;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
